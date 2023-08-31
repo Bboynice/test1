017294ffb92eb1b2284c4d1daac7b0def0db894b
Iliia <ilyatarasovskiy@gmail.com>
2023-09-01 00:04:36 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function ChangeFontSize(element, size) {
+  element.style.fontSize = size + 'px';
+}
+
+var text = document.getElementById('about_text');
+ChangeFontSize(text, 60); // Встановлюємо розмір шрифту 60px
+
+
+
+
+
+
